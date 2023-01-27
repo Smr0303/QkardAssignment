b9b880f9f982f50ab55f3e4f3df753180b864da3
Smr0303 <samarmohite7@gmail.com>
2023-01-27 13:45:50 +0530

    Started frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React,{useState,useEffect} from "react";
+import OwnerDashboard from "./components/OwnerDashboard";
+import UserDashboard from "./components/UserDashboard";
+
+
+function App() {
+  return (
+    <div className="App">
+      Sammy
+    </div>
+  );
+}
+
+export default App;
