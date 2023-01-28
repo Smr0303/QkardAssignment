e92f201324fcd8861968df4b3bb631305f4339fa
Smr0303 <samarmohite7@gmail.com>
2023-01-28 19:50:27 +0530

    OwnerDashboard updated

--- frontend/src/components/OwnerDashboard.js
+++ frontend/src/components/OwnerDashboard.js
@@ -1,26 +1,41 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { Button, Form, Modal } from "react-bootstrap";
 import { toast } from "react-toastify";
 
 function OwnerDashboard({ account, provider, CreditContract }) {
-  const [address, setAddress] = useState("SomeAddress");
+  const [address, setAddress] = useState("");
 
-  const [creditScore,setCreditScore] = useState(null);
-  const [paymentCount,setPaymentCount] = useState(0);
+  const [creditScore, setCreditScore] = useState(null);
+  const [paymentCount, setPaymentCount] = useState(null);
   const [creditLimit, setCreditLimit] = useState("");
   const [type, setType] = useState("");
   const [present, setPresent] = useState("");
 
-  const [show, setShow] = useState(true);
+  const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
 
   const accessCreditInfo = async () => {
+    setAddress(address);
     const signer = provider.getSigner();
     const creditInfo = await CreditContract.connect(
       signer
     ).accessCreditInfo_Owner(address);
+    const paymentHistory = await CreditContract.connect(signer).paymentHistory(
+      address
+    );
+    const _paymentValue = parseInt(paymentHistory);
+    const _creditScore = parseInt(creditInfo.creditScore);
+    const _creditLimit = parseInt(creditInfo.creditLimit);
+    const type = 1 ? "Business" : "Individual";
+    const isPresent = creditInfo.IsPresent;
     console.log(creditInfo);
+    setPaymentCount(_paymentValue);
+    setCreditScore(_creditScore);
+    setCreditLimit(_creditLimit);
+    setPresent(isPresent);
+    setType(type);
+    setShow(true);
   };
 
   const handleSubmit = async (e) => {
@@ -38,19 +53,45 @@ function OwnerDashboard({ account, provider, CreditContract }) {
     }
   };
 
-  const handleAccessCreditInfo = async (e) => {
-    setShow(true);
-    setAddress(address);
+  const handleCreditLimit = async (e) => {
+     e.preventDefault();
+     const signer = await provider.getSigner();
+     const typeValue = (type ==="Business")?1:0;
+     const tx = await CreditContract.connect(signer).updateCreditInfo_Owner(address,creditScore,creditLimit,typeValue);
+     toast.success("Updating... Please remain stanby")
+     const receipt = await tx.wait();
+     if(receipt) {
+      toast("Successfully updated all the Info");
+     }
+  };
+  const handlePaymentHistory = async (e) => {
+    const signer = await provider.getSigner();
+    const tx = await CreditContract.connect(signer).updatePaymentHistory(
+      address,
+      paymentCount
+    );
+    toast.success("Updating....Please remain stanby");
+    const receipt = await tx.wait();
+    if (receipt) {
+      toast("Payment history updated");
+    }
+  };
+  const calculateCreditScore = async (e) => {
+    const signer = await provider.getSigner();
+    const tx = await CreditContract.connect(signer).calculateCreditScore(
+      address
+    );
+    toast.success("Updating....Please remain stanby");
+    const receipt = await tx.wait();
+    if (receipt) {
+      const info = await CreditContract.connect(signer).accessCreditInfo(
+        address
+      );
+      setCreditScore(parseInt(info.creditScore));
+      toast("Credit score Updated");
+    }
   };
 
-  const handleCreditLimit = async (e) => {};
-  const handlePaymentHistory = async (e) => {};
-  const calculateCreditScore=async (e)=>{
-  }
-
-  useEffect(() => {
-
-  });
 
   return (
     <div>
@@ -61,8 +102,10 @@ function OwnerDashboard({ account, provider, CreditContract }) {
             type="string"
             placeholder="Enter Users Address"
             value={address}
-            onChange={(e) => {setAddress(e.target.value)
-            console.log(e.target.value)}}
+            onChange={(e) => {
+              setAddress(e.target.value);
+              console.log(e.target.value);
+            }}
           />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicPassword">
@@ -75,7 +118,7 @@ function OwnerDashboard({ account, provider, CreditContract }) {
           <Button
             variant="dark"
             style={{ padding: "10px 20px", margin: "10px 20px", width: "30vw" }}
-            onClick={handleAccessCreditInfo}
+            onClick={accessCreditInfo}
           >
             Access And Update Info
           </Button>
@@ -92,7 +135,7 @@ function OwnerDashboard({ account, provider, CreditContract }) {
               <Form.Label>User Address</Form.Label>
               <Form.Control
                 type="input"
-                value={address }
+                value={address}
                 placeholder="Your Address"
                 disabled
               />
@@ -101,37 +144,45 @@ function OwnerDashboard({ account, provider, CreditContract }) {
               <Form.Label>Credit Score</Form.Label>
               <Form.Control
                 type="input"
-                value={creditScore ? creditScore : "0"}
+                value={creditScore}
                 placeholder=""
                 disabled
               />
               <Button
-            style={{ padding: "10px 20px", margin: "10px 20px", width: "20vw" }}
-            onClick={calculateCreditScore}
-          >
-            Calculate credit Score
-          </Button>
+                style={{
+                  padding: "10px 20px",
+                  margin: "10px 20px",
+                  width: "20vw",
+                }}
+                onClick={calculateCreditScore}
+              >
+                Calculate credit Score
+              </Button>
             </Form.Group>
             <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label>Credit Limit</Form.Label>
               <Form.Control
                 type="input"
-                value={creditLimit ? creditLimit : "Your Address"}
+                value={creditLimit}
+                onChange={(e) => setCreditLimit(e.target.value)}
                 placeholder="Enter Credit Limit"
               />
-                <Button
-            style={{ padding: "10px 20px", margin: "10px 20px", width: "20vw" }}
-            onClick={handleCreditLimit}
-          >
-            Update Credit Limit
-          </Button>
+              <Button
+                style={{
+                  padding: "10px 20px",
+                  margin: "10px 20px",
+                  width: "20vw",
+                }}
+                onClick={handleCreditLimit}
+              >
+                Update Credit Limit
+              </Button>
             </Form.Group>
             <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label>User Type</Form.Label>
               <Form.Control
                 type="input"
-                value={type ? type : "Your Address"}
-                placeholder=""
+                value={type}
                 disabled
               />
             </Form.Group>
@@ -139,12 +190,21 @@ function OwnerDashboard({ account, provider, CreditContract }) {
               <Form.Label>Payment Count</Form.Label>
               <Form.Control
                 type="input"
-                value={paymentCount ?. paymentCount }
+                value={paymentCount}
+                onChange={(e) => setPaymentCount(e.target.value)}
                 placeholder="Enter total transactions by user"
               />
-            <Button variant="primary" style={{ padding: "10px 20px", margin: "10px 20px", width: "20vw" }} onClick={handlePaymentHistory}>
-            Update Payments
-          </Button>
+              <Button
+                variant="primary"
+                style={{
+                  padding: "10px 20px",
+                  margin: "10px 20px",
+                  width: "20vw",
+                }}
+                onClick={handlePaymentHistory}
+              >
+                Update Payments
+              </Button>
             </Form.Group>
           </Form>
         </Modal.Body>
@@ -152,7 +212,7 @@ function OwnerDashboard({ account, provider, CreditContract }) {
           <Button variant="secondary" onClick={handleClose}>
             Close
           </Button>
-          <Button variant="primary"  onClick={handleClose}>
+          <Button variant="primary" onClick={handleClose}>
             Save Changes
           </Button>
         </Modal.Footer>
